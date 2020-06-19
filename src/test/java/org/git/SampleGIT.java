90fdc67b037275c5314e10d6a2d8b28a5ce1ef47
Lavanya-26 <lavanya676767.gmail.com>
2020-06-19 12:42:08 +0530

    emp2 changes

--- src/test/java/org/git/SampleGIT.java
+++ src/test/java/org/git/SampleGIT.java
@@ -3,8 +3,6 @@ package org.git;
 public class SampleGIT {
 	public static void main(String[] args) {
 		System.out.println("master");
-		System.out.println("emp1");
-		
 		System.out.println("emp2");
 	}
 
